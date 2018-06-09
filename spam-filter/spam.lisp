9828ee49de1e9f72e099311bc6f2869bdbaf5c13
tominaga <tominaga.switch@gmail.com>
2018-06-09 15:24:46 +0900

    done spam

--- spam-filter/spam.lisp
+++ spam-filter/spam.lisp
@@ -125,3 +125,97 @@
       summing prob)
    1.0))
 
+(defun add-file-to-corpus (filename type corpus)
+  (vector-push-extend (list filename type) corpus))
+
+(defparameter *corpus* (make-array 1000 :adjustable t :fill-pointer 0))
+
+(defun add-directory-to-corpus (dir type corpus)
+  (dolist (filename (list-directory dir))
+    (add-file-to-corpus filename type)))
+
+(defun test-classifier (corpus testing-fraction)
+  (clear-database)
+  (let* ((shuffled (shuffle-vector corpus))
+         (size (length corpus))
+         (train-on (floor (* size (-1 testing-fraction)))))
+    (train-from-corpus shuffled :start 0 :end train-on)
+    (test-from-corpus shuffled :start train-on)))
+
+(defparameter *max-chars* (* 10 1024))
+
+(defun train-from-corpus (corpus &key (start 0) end)
+  (loop for idx from start below (or end (length corpus)) do
+       (destructuring-bind (file type) (aref corpus idx)
+         (train (start-of-file file *max-chars*) type))))
+
+(defun text-from-corpus (corpus &key (start 0) end)
+  (loop for idx from start below (or end (length corpus)) collect
+       (destructuring-bind (file type) (aref corpus idx)
+         (multiple-value-bind (classification score)
+             (classify (start-of-file file *max-chars*))
+           (list
+            :file file
+            :type type
+            :classification classification
+            :score score)))))
+
+(defun nshuffle-vector (vector)
+  (loop for idx downfrom (1- (length vector)) to 1
+     for other = (random (1 + idx))
+     do (unless (= idx other)
+          (rotatef (aref vector idx) (aref vector other))))
+  vector)
+
+(defun shuffle-vector (vector)
+  (nshuffle-vector (copy-seq vector)))
+
+(defun start-of-file (file max-chars)
+  (with-open-file (in file)
+    (let* ((length (min (file-length in) max-chars))
+           (text (make-string length))
+           (read (read-sequence text in)))
+      (if (< read length)
+          (subseq text 0 read)
+          text))))
+
+(defun result-type (result)
+  (destructuring-bind (&key type classification &allow-other-keys) result
+    (ecase type
+      (ham
+       (ecase classification
+         (ham 'correct)
+         (spam 'false-positive)
+         (unsure 'missed-ham)))
+      (spam
+       (ecase classification
+         (ham 'false-negative)
+         (spam 'correct)
+         (unsure 'missed-spam))))))
+
+(defun false-positive-p (result)
+  (eql (result-type result) 'false-positive))
+
+(defun false-negative-p (result)
+  (eql (result-type result) 'false-negative))
+
+(defun missed-ham-p (result)
+  (eql (result-type result) 'missed-ham))
+
+(defun missed-spam-p (result)
+  (eql (result-type result) 'missed-spam))
+
+(defun correct-p (result)
+  (eql (result-type result) 'correct))
+
+(defun analyze-results (results)
+  (let* ((keys '(total correct false-positive
+                 false-negative missed-ham missed-spam))
+         (counts (loop for x in keys collect (cons x 0))))
+    (dolist (item results)
+      (incf (cdr (assoc 'total counts)))
+      (incf (cdr (assoc (result-type item) counts))))
+    (loop with total = (cdr (assoc 'total counts))
+       for (label . count) in counts
+       do (format t "~&~@(~a~):~20t~5d~,5t: ~6,2f%~%"
+                  label count (* 100 (/ count total))))))
